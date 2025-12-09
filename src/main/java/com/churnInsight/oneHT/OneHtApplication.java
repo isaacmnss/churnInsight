32757f256d17c2b33d694b17100b5e73003496c1
Isaac Meneses Cabral de Argolo <isaaca@seed.se.gov>
2025-12-09 14:06:32 -0300

    chore: initial commit

---
+++ src/main/java/com/churnInsight/oneHT/OneHtApplication.java
@@ -0,0 +1,13 @@
+package com.churnInsight.oneHT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneHtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneHtApplication.class, args);
+	}
+
+}
