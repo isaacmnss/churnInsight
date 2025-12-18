ca0eb1b58e8b6974f2f72002dc36493bf3dab995
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2025-12-18 00:22:22 -0300

    feat: criação de DTO para receber os dados que serão utilizados pelo modelo para realizar a previsão

---
+++ src/main/java/dto/RequestPrevisaoDTO.java
@@ -0,0 +1,43 @@
+package dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RequestPrevisaoDTO(
+        @NotNull
+        Integer creditScore,
+
+        @NotBlank
+        String geography,
+
+        @NotBlank
+        String genero,
+
+        @NotNull
+        @Min(0)
+        Integer age,
+
+        @NotNull
+        Integer tenure,
+
+        @NotNull
+        Double balance,
+
+        @NotNull Integer numOfProducts,
+
+        @NotNull Integer hasCrCard,
+
+        @NotNull
+        Integer isActiveMember,
+
+        @NotNull
+        Double estimatedSalary,
+
+        @NotNull
+        Integer pointEarned,
+
+        @NotBlank
+        String cardType
+) {
+}
