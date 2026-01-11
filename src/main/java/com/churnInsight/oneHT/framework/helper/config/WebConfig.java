d779ad37a117836e6f131744717fe7aee7d2842f
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2026-01-11 14:04:09 -0300

    feat: config de CORS

---
+++ src/main/java/com/churnInsight/oneHT/framework/helper/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.churnInsight.oneHT.framework.helper.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedOrigins("https://localhost:3000", "https://localhost:8081", "https://localhost:8000")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
